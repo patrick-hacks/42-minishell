721e14a060cf556e25c1762e55bddf8c95035dc0
pfuchs <email@patrick-fuchs.com>
2022-10-09 04:06:28 +0200

    better cmake linking

--- src/signal.c
+++ src/signal.c
@@ -9,29 +9,29 @@
 // signal(SIGQUIT, SIG_IGN);
 
 //// asd
-void signal_heredoc() {
-  write(1, "\n", 1);
-  /// TODO: cancel heredoc
-}
+// void signal_heredoc() {
+//   write(1, "\n", 1);
+//   /// TODO: cancel heredoc
+// }
 
-void signal_readline() {
-  write(1, "\n", 1);
-  environ_add("exit=1");
-  rl_replace_line("", 0);
-  rl_on_new_line();
-  rl_redisplay();
-}
+// void signal_readline() {
+//   write(1, "\n", 1);
+//   environ_add("exit=1");
+//   rl_replace_line("", 0);
+//   rl_on_new_line();
+//   rl_redisplay();
+// }
 
-void signal_child() {
+// void signal_child() {
 
-}
+// }
 
-int signal_handler(void) {
-  struct termios term;
+// int signal_handler(void) {
+//   struct termios term;
 
-  signal(SIGINT, signal_child);
-  signal(SIGQUIT, signal_child);
-  tcgetattr(1, &term);
-  tcsetattr(1, 0, &term);
-  return (0);
-}
+//   signal(SIGINT, signal_child);
+//   signal(SIGQUIT, signal_child);
+//   tcgetattr(1, &term);
+//   tcsetattr(1, 0, &term);
+//   return (0);
+// }
